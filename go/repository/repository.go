a37097d8bd3d250f29c3744c4c183364796f0e60
unknown <lraimundo409@gmail.com>
2026-04-08 18:04:02 -0300

    fazendo a repository

--- go/repository/repository.go
+++ go/repository/repository.go
@@ -37,8 +37,105 @@ func (r *Repository) GetDeck(id int) (model.Response, error) {
 		fmt.Printf("Erro ao Buscar card do Deck : %v\n", err)
 		return model.Response{}, err
 	}
-	for _, cardDeck := range card {
+	var listCard []string
+	for card.Next() {
+		var cardUnit string
+		if err = card.Scan(&cardUnit); err != nil {
+			fmt.Printf("Erro ao Buscar card do Deck : %v\n", err)
+			return model.Response{}, err
+		}
+		listCard = append(listCard, cardUnit)
+	}
+
+	for _, crd = range listCard {
 
 	}
 
+}
+
+func (r *Repository) GetCardPokemon(id int) (model.Pokemon, error) {
+	var pokemon model.Pokemon
+	query := `SELECT id, nome, card_type, tipo, estagio, geracao, ps, recuo, fraqueza FROM pokemon WHERE id=$1`
+	err := r.db.QueryRow(query, id).Scan(&pokemon.Id, &pokemon.Nome, &pokemon.TipoCarta, &pokemon.Tipo, &pokemon.Estagio, &pokemon.Geracao, &pokemon.PS, &pokemon.Recuo, &pokemon.Fraqueza)
+	if err != nil {
+		fmt.Printf("Erro ao Buscar card do Deck : %v\n", err)
+		return model.Pokemon{}, err
+	}
+	query = `SELECT ataque FROM pokemon_ataque WHERE id_pokemon=$1`
+	ataque, err := r.db.Query(query, id)
+	if err != nil {
+		fmt.Printf("Erro ao Buscar ataque do pokemon: %v\n", err)
+		return model.Pokemon{}, err
+	}
+	var listAtaque []string
+	for ataque.Next() {
+		var ataqueUnit string
+		if err = ataque.Scan(&ataqueUnit); err != nil {
+			fmt.Printf("Erro ao Buscar ataque do pokemon: %v\n", err)
+			return model.Pokemon{}, err
+		}
+		listAtaque = append(listAtaque, ataqueUnit)
+	}
+
+	for _, atk := range listAtaque {
+		var valueAtaque model.Ataque
+		query = `SELECT nome_ataque, dano_ataque, custo_ataque, efeito_ataque FROM ataque WHERE nome_ataque=$1`
+		err = r.db.QueryRow(query, atk).Scan(&valueAtaque.Nome, &valueAtaque.Dano, &valueAtaque.Custo, &valueAtaque.Efeito)
+		if err != nil {
+			fmt.Printf("Erro ao Buscar ataque do pokemon: %v\n", err)
+			return model.Pokemon{}, err
+		}
+		pokemon.Ataque = append(pokemon.Ataque, valueAtaque)
+	}
+
+	query = `SELECT habilidade FROM pokemon_habilidade WHERE id_pokemon=$1`
+	habilidade, err := r.db.Query(query, id)
+	if err != nil {
+		fmt.Printf("Erro ao Buscar habilidade do pokemon: %v\n", err)
+		return model.Pokemon{}, err
+	}
+	var listHabilidade []string
+	for habilidade.Next() {
+		var habilidadeUnit string
+		if err = habilidade.Scan(&habilidadeUnit); err != nil {
+			fmt.Printf("Erro ao Buscar habilidade do pokemon: %v\n", err)
+			return model.Pokemon{}, err
+		}
+		listHabilidade = append(listHabilidade, habilidadeUnit)
+	}
+
+	for _, atk := range listHabilidade {
+		var valueHabilidade model.Habilidade
+		query = `SELECT nome_habilidade, efeito_habilidade FROM habilidade WHERE nome_habilidade=$1`
+		err = r.db.QueryRow(query, atk).Scan(&valueHabilidade.Nome, &valueHabilidade.Efeito)
+		if err != nil {
+			fmt.Printf("Erro ao Buscar Habilidade do pokemon: %v\n", err)
+			return model.Pokemon{}, err
+		}
+		pokemon.Habilidade = append(pokemon.Habilidade, valueHabilidade)
+	}
+
+	return pokemon, err
+}
+
+func (r *Repository) GetCardApoiador(id int) (model.Apoiador, error) {
+	var apoiador model.Apoiador
+	query := `SELECT id, nome, card_type, efeito FROM apoiador WHERE id=$1`
+	err := r.db.QueryRow(query, id).Scan(&apoiador.Id, &apoiador.Nome, &apoiador.CardType, &apoiador.Efeito)
+	if err != nil {
+		fmt.Printf("Erro ao Buscar card do Deck : %v\n", err)
+		return model.Apoiador{}, err
+	}
+	return apoiador, err
+}
+
+func (r *Repository) GetCardItem(id int) (model.Item, error) {
+	var item model.Item
+	query := `SELECT id, nome, card_type, efeito FROM apoiador WHERE id=$1`
+	err := r.db.QueryRow(query, id).Scan(&item.Id, &item.Nome, &item.CardType, &item.Efeito)
+	if err != nil {
+		fmt.Printf("Erro ao Buscar card do Deck : %v\n", err)
+		return model.Item{}, err
+	}
+	return item, err
 }
---
+++ go/resource/migration.sql
@@ -0,0 +1,53 @@
+CREATE TABLE IF NOT EXISTS deck (
+    id SERIAL PRIMARY KEY,
+    quantidade INT NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    estatistica INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS deck_card (
+    id_deck INT NOT NULL,
+    card_deck INT NOT NULL,
+    PRIMARY KEY (id_deck, card_deck),
+    FOREIGN KEY (id_deck) REFERENCES deck(id),
+    FOREIGN KEY (card_deck) REFERENCES cards (card_deck)
+);
+
+CREATE TABLE IF NOT EXISTS cards (
+    id SERIAL PRIMARY KEY,
+    type_card VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS cards_pokemon (
+    id_card INT NOT NULL,
+    card_pokemon INT NOT NULL,
+    PRIMARY KEY (id_card, card_pokemon),
+    FOREIGN KEY (id_card) REFERENCES cards(id_card),
+    FOREIGN KEY (card_pokemon) REFERENCES pokemon (id)
+);
+
+CREATE TABLE IF NOT EXISTS cards_apoiador (
+    id_card INT NOT NULL,
+    card_apoiador INT NOT NULL,
+    PRIMARY KEY (id_card, card_apoiador),
+    FOREIGN KEY (id_card) REFERENCES cards(id_card),
+    FOREIGN KEY (card_apoiador) REFERENCES apoiador (id)
+);
+
+CREATE TABLE IF NOT EXISTS cards_item (
+    id_card INT NOT NULL,
+    card_item INT NOT NULL,
+    PRIMARY KEY (id_card, card_item),
+    FOREIGN KEY (id_card) REFERENCES cards(id_card),
+    FOREIGN KEY (card_item) REFERENCES item (id)
+);
+
+CREATE TABLE IF NOT EXISTS estatistica (
+    id SERIAL PRIMARY KEY,
+    vitoria INT NOT NULL,
+    derrota INT NOT NULL,
+    total INT NOT NULL,
+    pontos_ganho INT NOT NULL,
+    pontos_perdido INT NOT NULL,
+    media_pontos DECIMAL(10,2) NOT NULL,
+);
